070691a3127f9d58a6072f7b012afbdac3df0576
Teodor Morosanu <morosanuteodor@gmail.com>
2023-05-23 20:55:38 +0300

    MotionBox : Added project final touches

--- Projects/MotionBox/Code/Receiver/Receiver.ino
+++ Projects/MotionBox/Code/Receiver/Receiver.ino
@@ -1,44 +1,42 @@
 #include <SPI.h>
-#include <nRF24L01.h>
+#include <nRF24L01.h> // Libraries for nRF24L01 transmitter
 #include <RF24.h>
 
-RF24 radio(7,8);
+RF24 radio(7,8); // Initialising RF24
 
-void LEDsSetup(int redPin, int yellowPin, int greenPin) {
+void LEDsSetup(int redPin, int yellowPin, int greenPin) { // Method for LEDs setup
   pinMode(redPin, OUTPUT);
-  pinMode(yellowPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT); // Setting the LED pins as output
   pinMode(greenPin, OUTPUT);
 }
 
-void BuzzerSetup(int vccPin, int iPin) {
-  pinMode(iPin, OUTPUT);
-  pinMode(vccPin, OUTPUT);
+void BuzzerSetup(int vccPin, int iPin) { // Method for buzzer setup
+  pinMode(iPin, OUTPUT); // Setting the buzzer pin as output
+  pinMode(vccPin, OUTPUT); // Powering the buzzer from another arduino pin
 }
 
-void ReceiverSetup(byte address[]) {
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_250KBPS);
-  radio.startListening();
+void ReceiverSetup(byte address[]) { // Method for receiver setup
+  radio.begin(); // Radio beginning
+  radio.openReadingPipe(0, address); // Getting the adress
+  radio.setPALevel(RF24_PA_MAX); // Setting the PA level
+  radio.setDataRate(RF24_250KBPS); // Setting the data rate
+  radio.startListening();// Starting the receiver listening
 }
 
 void setup() {
-  Serial.begin(9600);
-  LEDsSetup(A0, A1, A2);
-  BuzzerSetup(3, 2);
-  ReceiverSetup("00001");
-  digitalWrite(A1, HIGH);
+  LEDsSetup(A0, A1, A2); // Method for LEDs setup
+  BuzzerSetup(3, 2); // Method for buzzer setup
+  ReceiverSetup("00001"); // Method for receiver setup
+  digitalWrite(A1, HIGH); // Powering the yellow LED
 }
 
-void Receiver(int iPin) {
+void Receiver(int iPin) { // Method for receiving and processing the data
   char text[32] = "";
-  if (radio.available()) {
+  if (radio.available()) { // If a radio signal is available, a text is read
     digitalWrite(A1, LOW);
     radio.read(&text, sizeof(text));
     String transData = String(text);
-    if(transData == "verify") {
-      Serial.println("Te-am prins");
+    if(transData == "verify") { // If the readed text is "verify", the buzzer and red LED are turned on
       digitalWrite(A2, LOW);
       digitalWrite(3, HIGH);
       digitalWrite(A0, HIGH);
@@ -49,14 +47,13 @@ void Receiver(int iPin) {
       delay(500);
       digitalWrite(A0, HIGH);
     }
-    Serial.println("Liber");
     digitalWrite(A2, HIGH);
-    digitalWrite(A0, LOW);
+    digitalWrite(A0, LOW); // Powering on the green LED and powering off the red LED and the buzzer if no text is received
     digitalWrite(3, LOW);
     noTone(iPin);  
   }
 }
 
-void loop() {
+void loop() { // Method for receiving and processing the data
   Receiver(2);
 }
--- Projects/MotionBox/Code/Transmitter/Transmitter.ino
+++ Projects/MotionBox/Code/Transmitter/Transmitter.ino
@@ -1,43 +1,41 @@
 #include <SPI.h>
-#include <nRF24L01.h>
+#include <nRF24L01.h> // Libraries for nRF24L01 transmitter
 #include <RF24.h>
 
-RF24 radio(7,8);
+RF24 radio(7,8); // Initialising RF24
 
-int sensorValue;
+int sensorValue; // Variable for storing the PIR sensor value
 
-void PirSetup(int sensorPin) {
-  pinMode(sensorPin, INPUT);
+void PirSetup(int sensorPin) { // Method for PIR setup
+  pinMode(sensorPin, INPUT); // Setting the sensorPin as input
 }
 
-void TransmitterSetup(byte address[]) {
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_250KBPS);
-  radio.stopListening();
+void TransmitterSetup(byte address[]) { // Method for transmitter setup
+  radio.begin(); // Radio beginning
+  radio.openWritingPipe(address); // Setting the adress
+  radio.setPALevel(RF24_PA_MAX); // Setting the PA level
+  radio.setDataRate(RF24_250KBPS); // Setting the data rate
+  radio.stopListening(); // Stopping the transmitter listening
 }
 
 void setup() {
-  Serial.begin(9600);
-  PirSetup(3);
-  TransmitterSetup("00001");
+  PirSetup(3); // Method for PIR setup
+  TransmitterSetup("00001"); // Method for transmitter setup
 }
 
-void Pir() {
-  sensorValue = digitalRead(3);
-  Serial.println(sensorValue);
+void Pir() { // Method for getting the PIR sensor value
+  sensorValue = digitalRead(3); // Getting the sensor voltage (HIGH / LOW)
 }
 
-void Transmitter() {
-  if(sensorValue == HIGH) {
+void Transmitter() { // Method for transmitting the data
+  if(sensorValue == HIGH) { // If PIR detects movement a "verify" text is transmitted
     const char text[] = "verify";
     radio.write(&text, sizeof(text));
   }
 }
 
 void loop() {
-  Pir();
-  Transmitter();
+  Pir(); // Method for getting the PIR sensor value
+  Transmitter(); // Method for transmitting the data
 }
 
