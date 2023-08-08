1a79764fcf1536ec96445aa12c582201b7658a0e
Teodor Morosanu <morosanuteodor@gmail.com>
2023-08-08 20:21:27 +0300

    FreshenerDispenser : Added assembly

--- Projects/FreshenerDispenser/Code/Dispenser/Dispenser.ino
+++ Projects/FreshenerDispenser/Code/Dispenser/Dispenser.ino
@@ -5,6 +5,7 @@ RTC_PCF8563 rtc;
 int motorIn = 11;
 
 void setup () {
+  Serial.begin(9600);
   Wire.begin(); // Wire begin
   rtc.begin(); // Real time clock begin
   rtc.adjust(DateTime(__DATE__, __TIME__)); // Adjusting the RTC (computer date)
@@ -13,9 +14,10 @@ void setup () {
 
 void loop () {
   DateTime now = rtc.now(); // Getting the RTC time
-  if(now.minute() == 0 && now.second() <= 5) { // At each hour, for 5 seconds, the motor spins
-    analogWrite(motorIn, 250);
+  if(now.minute() == 59 && now.second() >= 55) { // At each hour, for 5 seconds, the motor spins
+    digitalWrite(motorIn, HIGH);
   } else {
-    analogWrite(motorIn, 0);
+    digitalWrite(motorIn, LOW);
   }
+  Serial.println(now.second());
 }
