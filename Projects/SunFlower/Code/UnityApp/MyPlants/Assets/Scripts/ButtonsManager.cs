7c672740e51caa9790317cdf223bbd4b81edc98e
Teodor Morosanu <morosanuteodor@gmail.com>
2025-10-26 16:30:28 +0200

    TrainNetwork : Added EA-236 locomotive 3D models

--- Projects/SunFlower/Code/UnityApp/MyPlants/Assets/Scripts/ButtonsManager.cs
+++ Projects/SunFlower/Code/UnityApp/MyPlants/Assets/Scripts/ButtonsManager.cs
@@ -64,22 +64,6 @@ public class ButtonsManager : MonoBehaviour, IPointerUpHandler {
         StartCoroutine(WateringCoroutine());
     }
 
-    private void WiFiNetworks() {
-        #if UNITY_ANDROID
-        try {
-            using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
-                AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-                using (AndroidJavaObject intent = new AndroidJavaObject("android.content.Intent", "android.settings.WIFI_SETTINGS")) {
-                    currentActivity.Call("startActivity", intent);
-                }
-            }
-        }
-        catch (System.Exception exception) {
-            Debug.LogError("Failed to open Wi-Fi settings: " + exception.Message);
-        }
-        #endif
-    }
-
     public void NewAp() {
         StartCoroutine(NewApCoroutine());
         //WiFiNetworks();
